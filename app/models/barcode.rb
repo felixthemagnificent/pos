18e5df0b58cfa47773dcc9832bd8ef2cfd8f750f
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2017-04-20 13:46:01 +0500

    fix notexistent method

--- app/models/barcode.rb
+++ app/models/barcode.rb
@@ -3,4 +3,8 @@ class Barcode < ApplicationRecord
   validates :count, presence: true
   validates :code, presence: true
   scope :for_user, ->(user) { joins(:item).where(items: { user: user} ) }
+
+  def in_stock
+    self.count > 0
+  end
 end
